c8896eaed57c971a5f5afe892d9a5ed95eb89fc9
visa <ahvisa1@gmail.com>
2023-06-22 02:52:35 -0700

    added a function

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By 
 import argparse
 import requests
 import os
 import time
 import shutil
 
-def download_all_images(url, directoryName):
+def download_all_images_by_excecuting_the_script(url, directoryName):
+    #set chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
     #Selenium v4+ must be used Service
     service = Service(executable_path='C:/Users/Visa/Dropbox/Document In The US/Etsy/python_code/chromedriver.exe')  # point this to your ChromeDriver location
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver.get(url)
 
@@ -31,7 +37,48 @@ def download_all_images(url, directoryName):
             out_file.write(response.content)
         print(f"Downloaded image_{i}.jpg")
 
-    driver.quit()   
+    driver.quit()  
+
+def download_all_images_by_scrapping(url, directoryName):
+    def trim_url(url):
+        index = url.find(".jpg")
+        if index != -1:
+            return url[:index+4]
+        
+    #set chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")       
+
+    # setup the webdriver
+    service = Service(executable_path='C:/Users/Visa/Dropbox/Document In The US/Etsy/python_code/chromedriver.exe')  # point this to your ChromeDriver location
+    driver = webdriver.Chrome(service=service,options=chrome_options)
+    driver.implicitly_wait(5)
+
+    driver.get(url)
+
+    time.sleep(5)
+
+    content = driver.find_element(By.CLASS_NAME,'sku-property-list')
+    imgs = content.find_elements(By.TAG_NAME, "li")
+
+    imgs_url = []
+    for i in imgs:
+        img_url = trim_url(i.find_element(By.TAG_NAME,'div').find_element(By.TAG_NAME,'img').get_attribute('src'))
+        imgs_url.append(img_url)
+
+    # define the directory for storing images
+    img_dir = os.path.join(os.getcwd(),directoryName)  
+
+    # Check if directory exist or not -> if exist -> Empty it otherwise create it
+    createDirectoryOrEmptyIt(img_dir)
+
+    for i, url in enumerate(imgs_url):
+        response = requests.get(url, stream=True)
+        with open(os.path.join(img_dir, f'image_{i}.jpg'), 'wb') as out_file:
+            out_file.write(response.content)
+        print(f"Downloaded image_{i}.jpg")
+
+    driver.quit()
 
 def createDirectoryOrEmptyIt(directory):
     path_to_directory = os.path.join(os.getcwd(),directory)
@@ -50,8 +97,12 @@ def createDirectoryOrEmptyIt(directory):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download all images from Aliexpress")
-    parser.add_argument("--url", help="URL of the product")
-    parser.add_argument("--directory", help="The directory that image will be stored")
-    
+    parser.add_argument("--url",type=str, help="URL of the product")
+    parser.add_argument("--directory",type=str, help="The directory that image will be stored")
+
     args = parser.parse_args()
-    download_all_images(args.url,args.directory)
+    
+    if not args.url or not args.directory:
+        parser.error("url and directory cannot be empty")
+
+    download_all_images_by_scrapping(args.url,args.directory)
